d147988a605217ccea2187c1a517acbe5f7f75c0
Anya <75702826+anyacherniss@users.noreply.github.com>
2025-05-19 16:52:34 -0400

    update banner copy

--- client/src/app/landing/components/DeveloperBanner.tsx
+++ client/src/app/landing/components/DeveloperBanner.tsx
@@ -12,7 +12,10 @@ export function DeveloperBanner(props: DeveloperBannerProps) {
     <div className='flex w-full h-9 bg-indigo-500 text-white justify-between items-center px-4 flex-shrink-0'>
       <div />
       <div className='flex flex-row gap-2 items-center'>
-        <span className='text-white text-[13px] font-medium'>Introducing WorkflowAI for Developers!</span>
+        <span className='text-white text-[13px] font-normal'>
+          <span className='font-semibold'>Introducing WorkflowAI for Developers:</span> Bring the benefits of WorkflowAI
+          to your existing agents with just 2 lines of code.
+        </span>
         {customLink && (
           <Link href={customLink} className='text-white text-[13px] font-medium underline'>
             {linkText}
